2ab21e5572f50d8b67b0e1daef99bb70d5cd3a6d
palanimohand <palanimohan.d@outlook.com>
2021-06-05 16:17:17 +0530

    calculator program addition

---
+++ java-training/src/BasicCalculator.java
@@ -0,0 +1,16 @@
+
+public class BasicCalculator {
+
+	public static void main(String[] args) {
+
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		
+		System.out.println("Addition Result : "+(a+b));
+		System.out.println("Subtraction Result : "+(a-b));
+		System.out.println("Multiplication Result : "+(a*b));
+		System.out.println("Division Result : "+(a/b));
+		
+	}
+
+}
