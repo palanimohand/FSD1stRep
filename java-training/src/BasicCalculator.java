7816f2b3d3d1a9cafa228bbe3457a2ffdb6bb84e
palanimohand <palanimohan.d@outlook.com>
2021-06-06 12:45:25 +0530

    Extra methods in conversion from string to int

--- java-training/src/BasicCalculator.java
+++ java-training/src/BasicCalculator.java
@@ -4,8 +4,13 @@ public class BasicCalculator {
 	public static void main(String[] args) {
 
 		int a = Integer.parseInt(args[0]);
+		System.out.println(a);
 		int b = Integer.parseInt(args[1]);
-		
+		System.out.println(b);
+		int c = Integer.valueOf(args[0]);
+		System.out.println(c);
+		int d = Integer.valueOf(args[1]);
+		System.out.println(d);
 		System.out.println("Addition Result : "+(a+b));
 		System.out.println("Subtraction Result : "+(a-b));
 		System.out.println("Multiplication Result : "+(a*b));
